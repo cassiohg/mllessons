e3c30a700ed6b8369d7b3a4a4cffe9044e5f944b
Cassiohg <cassiohg@gmail.com>
2018-07-29 22:27:37 -0300

    created target function and 100 points randomly. Classified all points using target function.

--- pla.go
+++ pla.go
@@ -3,10 +3,45 @@ package main
 import (
 	"fmt"
 	// "time"
-	// "math"
+	"math/rand"
 )
 
+type Point struct {
+	x, y	float32
+	class 	int
+}
+
+type TargetFunction struct {
+	p1, p2 		Point
+	slope, b	float32
+}
+
+func (f *TargetFunction) calculateFunctionFromPoints () {
+	f.slope = (f.p2.y - f.p1.y) / (f.p2.x - f.p1.x)
+	f.b = f.p1.y - f.slope*f.p1.x
+}
+
+func (f TargetFunction) classifyPoint (p *Point) int {
+	if p.y > f.slope*p.x + f.b { p.class = 1 } else { p.class = -1 }
+	return p.class
+}
+
 func main() {
-	fmt.Pritnln("Assignments for computer intelligence course on UFRJ 2018")
+	fmt.Println("Assignments for computer intelligence course on UFRJ 2018\n")
+
+	rand.Seed(0)
+
+	f := TargetFunction{
+		p1: Point{x: rand.Float32(), y: rand.Float32()}, 
+		p2: Point{x: rand.Float32(), y: rand.Float32()}}
+	f.calculateFunctionFromPoints()
+
+	amountOfPoints := 100
+	points := make([]Point, amountOfPoints)
+	for i := range points {
+		points[i].x, points[i].y = rand.Float32(), rand.Float32()
+		f.classifyPoint(&points[i])
+	}
+
 }
 
